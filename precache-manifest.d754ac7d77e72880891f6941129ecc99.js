36f3bb56b36b6bdbbd3d2a17f91f76a966de8659
Benvinda Vedes <bafonso.vedes@gmail.com>
2020-10-24 10:31:11 +0100

    Updates

---
+++ precache-manifest.d754ac7d77e72880891f6941129ecc99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4b00d3c1a8ecf2c7666ae867720fac",
+    "url": "/build-markdown-previewer/index.html"
+  },
+  {
+    "revision": "bcc1062360b9afc2bf6e",
+    "url": "/build-markdown-previewer/static/css/main.572f201a.chunk.css"
+  },
+  {
+    "revision": "85947d11a2884d56a604",
+    "url": "/build-markdown-previewer/static/js/2.4cd925e1.chunk.js"
+  },
+  {
+    "revision": "cb7f2d355156ae0bf3c833060a320e80",
+    "url": "/build-markdown-previewer/static/js/2.4cd925e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc1062360b9afc2bf6e",
+    "url": "/build-markdown-previewer/static/js/main.24aa3274.chunk.js"
+  },
+  {
+    "revision": "da2bc05f5bbf629151c1",
+    "url": "/build-markdown-previewer/static/js/runtime-main.7b431e60.js"
+  }
+]);
